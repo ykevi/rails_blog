2f0de5e60e60c2098c43b475e6ec08e11df4dc4a
ykevi <ykevi94@gmail.com>
2018-06-23 16:26:04 +0900

    投稿機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
     # GET /posts/new
     def new
     end
+
+    # POST /posts
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
